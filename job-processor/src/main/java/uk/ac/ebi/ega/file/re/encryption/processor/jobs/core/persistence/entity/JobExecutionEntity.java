bf86eafd4ec68119669188773fcb22d5b943b8bf
jorizci <jorizci@gmail.com>
2019-05-20 17:08:44 +0100

    Modified imports to do not use the .* form

--- job-processor/src/main/java/uk/ac/ebi/ega/file/re/encryption/processor/jobs/core/persistence/entity/JobExecutionEntity.java
+++ job-processor/src/main/java/uk/ac/ebi/ega/file/re/encryption/processor/jobs/core/persistence/entity/JobExecutionEntity.java
@@ -21,7 +21,13 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
